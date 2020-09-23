56ae5e7894ac4b8fd0dcb3e01587b3e3e86ba956
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2020-09-23 14:18:10 +0530

    updated title in appbar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,6 @@ void main() {
 class  MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Tech-Gram Academy"),),);
+    return Scaffold(appBar: AppBar(title: Text("Tech-Gram Academy App"),),);
   }
 }
